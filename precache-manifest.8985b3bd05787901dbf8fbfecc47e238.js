f4f062efb1249f04cd14a8c0909440f6d7c42122
Dev Behera <devj1988@gmail.com>
2020-08-16 18:00:39 -0500

    Updates

---
+++ precache-manifest.8985b3bd05787901dbf8fbfecc47e238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e217e5064c58b9f54012cc11fbe3ae",
+    "url": "/spellingBeeApp/index.html"
+  },
+  {
+    "revision": "aef2da4d9b8e3ea91b1c",
+    "url": "/spellingBeeApp/static/css/main.ed27be23.chunk.css"
+  },
+  {
+    "revision": "4ae45661650b721cae4b",
+    "url": "/spellingBeeApp/static/js/2.aa83ad28.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/spellingBeeApp/static/js/2.aa83ad28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef2da4d9b8e3ea91b1c",
+    "url": "/spellingBeeApp/static/js/main.1189c04c.chunk.js"
+  },
+  {
+    "revision": "4095812e9408979dd0d3",
+    "url": "/spellingBeeApp/static/js/runtime-main.136e0d51.js"
+  },
+  {
+    "revision": "a4d298652cef9e248d11b4dd24ebd744",
+    "url": "/spellingBeeApp/static/media/refresh-svgrepo-com.a4d29865.svg"
+  }
+]);
